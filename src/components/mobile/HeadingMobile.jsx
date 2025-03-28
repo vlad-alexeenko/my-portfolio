b76f0c466b664eb1bd00622dc12b193d61396d8b
Vlad Alexeenko <valexeenko74@gmail.com>
2025-03-27 22:34:57 -0700

    mobile margin resolution changes

--- src/components/mobile/HeadingMobile.jsx
+++ src/components/mobile/HeadingMobile.jsx
@@ -31,7 +31,7 @@ function HeadingMobile({scrollToAboutMe}) {
     return(
         <section className="">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7" style={{marginTop: '4vh'}}>
+        <div className="mr-auto place-self-center lg:col-span-7" style={{marginTop: '2vh'}}>
         <div className="flex justify-center mt-6 lg:mt-0 lg:col-span-5" style={{marginBottom: '3rem'}}>
             <img src={MyPicture} alt="mockup"/>
         </div> 
